19604ad2709a62f5421eb30f5ae13d82189cadd9
Jihoon <godpd440@naver.com>
2025-11-24 10:18:48 +0900

    skeleton

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,15 @@
 export const contractAddress = '0xa34c43912491681e82a94e7d2c15d70afd337633'
+
+export const SEPOLIA_CHAIN_ID = 11155111
+
+export const SEPOLIA_NETWORK = {
+  chainId: `0x${SEPOLIA_CHAIN_ID.toString(16)}`,
+  chainName: 'Sepolia',
+  nativeCurrency: {
+    name: 'Ether',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  rpcUrls: ['https://sepolia.infura.io/v3/'],
+  blockExplorerUrls: ['https://sepolia.etherscan.io/'],
+}
